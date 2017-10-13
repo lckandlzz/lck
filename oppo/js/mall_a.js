70eebf4f8d2be0b5f135e43f16edb595428e7d66
lckandlzz <228655196@qq.com>
2017-10-13 13:31:56 +0800

    kb
    op

---
+++ oppo/js/mall_a.js
@@ -0,0 +1,15 @@
+$(function(){
+	setTimeout(function(){
+		 var h = $(document).height()-$(window).height();
+		var step=0.5;
+		setInterval(function(){
+			$(document).scrollTop(step);
+			step=step+0.5;
+			if(step>=h){
+				setTimeout(function(){
+					window.location.href='shop.html'
+				},2000)
+			}
+		},1);
+	},3000);
+});
